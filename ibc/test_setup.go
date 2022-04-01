de559dfd7c025c95ea5348b4e9a62f600f5267c2
Andrew Gouin <andrew@gouin.io>
2022-04-01 16:49:25 -0600

    Log container log tails regardless of error

--- ibc/test_setup.go
+++ ibc/test_setup.go
@@ -77,21 +77,21 @@ func CreateTestNetwork(pool *dockertest.Pool, name string, t *testing.T) (*docke
 func Cleanup(t *testing.T, pool *dockertest.Pool, testDir string) func() {
 	return func() {
 		testName := t.Name()
-		testFailed := t.Failed()
+		// testFailed := t.Failed()
 		cont, _ := pool.Client.ListContainers(docker.ListContainersOptions{All: true})
 		ctx := context.Background()
 		for _, c := range cont {
 			for k, v := range c.Labels {
 				if k == "ibc-test" && v == testName {
 					_ = pool.Client.StopContainer(c.ID, 10)
-					_, err := pool.Client.WaitContainerWithContext(c.ID, ctx)
-					if err != nil || testFailed {
-						stdout := new(bytes.Buffer)
-						stderr := new(bytes.Buffer)
-						_ = pool.Client.Logs(docker.LogsOptions{Context: ctx, Container: c.ID, OutputStream: stdout, ErrorStream: stderr, Stdout: true, Stderr: true, Tail: "50", Follow: false, Timestamps: false})
-						names := strings.Join(c.Names, ",")
-						fmt.Printf("{%s} - stdout:\n%s\n{%s} - stderr:\n%s\n", names, stdout, names, stderr)
-					}
+					_, _ = pool.Client.WaitContainerWithContext(c.ID, ctx)
+					// if err != nil || testFailed {
+					stdout := new(bytes.Buffer)
+					stderr := new(bytes.Buffer)
+					_ = pool.Client.Logs(docker.LogsOptions{Context: ctx, Container: c.ID, OutputStream: stdout, ErrorStream: stderr, Stdout: true, Stderr: true, Tail: "50", Follow: false, Timestamps: false})
+					names := strings.Join(c.Names, ",")
+					fmt.Printf("{%s} - stdout:\n%s\n{%s} - stderr:\n%s\n", names, stdout, names, stderr)
+					// }
 					_ = pool.Client.RemoveContainer(docker.RemoveContainerOptions{ID: c.ID})
 				}
 			}
