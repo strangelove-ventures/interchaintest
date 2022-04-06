21811707e2b00652f08be6cb579c3ac1c7ad05c4
Andrew Gouin <andrew@gouin.io>
2022-04-05 19:29:54 -0600

    Simplify test

--- ibc/test_contract.go
+++ ibc/test_contract.go
@@ -23,7 +23,7 @@ func (ibc IBCTestCase) JunoHaltTest(testName string, srcChain Chain, dstChain Ch
 
 	srcChainCfg := srcChain.Config()
 
-	// Generate key to be used for "user" that will execute IBC transaction
+	// Generate key to be used for "user" that will execute transactions
 	if err := srcChain.CreateKey(ctx, userAccountKeyName); err != nil {
 		return err
 	}
@@ -38,7 +38,7 @@ func (ibc IBCTestCase) JunoHaltTest(testName string, srcChain Chain, dstChain Ch
 		return err
 	}
 
-	// Fund user account on src chain in order to relay from src to dst
+	// Fund user account on src chain that will be used to instantiate and execute contract
 	userWalletSrc := WalletAmount{
 		Address: userAccountSrc,
 		Denom:   srcChainCfg.Denom,
@@ -56,48 +56,44 @@ func (ibc IBCTestCase) JunoHaltTest(testName string, srcChain Chain, dstChain Ch
 	rootPath := filepath.Dir(executablePath)
 	contractPath := path.Join(rootPath, "assets", "badcontract.wasm")
 
-	contractAddress, err := srcChain.InstantiateContract(ctx, userAccountKeyName, WalletAmount{Amount: 100, Denom: srcChain.Config().Denom}, contractPath, "{\"count\":0}", false)
+	contractAddress, err := srcChain.InstantiateContract(ctx, userAccountKeyName, WalletAmount{Amount: 100, Denom: srcChain.Config().Denom}, contractPath, "{\"count\":0}", srcChainCfg.Version == "v2.3.0")
 	if err != nil {
 		return err
 	}
 
 	resets := []int{0, 15, 84, 0, 84, 42, 55, 42, 15, 84, 42}
 
-	for i := 0; i < 10; i++ {
-		for _, resetCount := range resets {
-			// run reset
-			if err := srcChain.ExecuteContract(ctx, userAccountKeyName, contractAddress, fmt.Sprintf("{\"reset\":{\"count\": %d}}", resetCount)); err != nil {
-				return err
-			}
-			latestHeight, err := srcChain.WaitForBlocks(5)
-			if err != nil {
-				return err
-			}
+	for _, resetCount := range resets {
+		// run reset
+		if err := srcChain.ExecuteContract(ctx, userAccountKeyName, contractAddress, fmt.Sprintf("{\"reset\":{\"count\": %d}}", resetCount)); err != nil {
+			return err
+		}
+		latestHeight, err := srcChain.WaitForBlocks(5)
+		if err != nil {
+			return err
+		}
 
-			// dump current contract state
-			res, err := srcChain.DumpContractState(ctx, contractAddress, latestHeight)
-			if err != nil {
-				return err
-			}
-			contractData, err := base64.StdEncoding.DecodeString(res.Models[1].Value)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("Contract data: %s\n", contractData)
+		// dump current contract state
+		res, err := srcChain.DumpContractState(ctx, contractAddress, latestHeight)
+		if err != nil {
+			return err
+		}
+		contractData, err := base64.StdEncoding.DecodeString(res.Models[1].Value)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Contract data: %s\n", contractData)
 
-			// run increment a bunch of times
-			for i := 0; i < 5; i++ {
-				if err := srcChain.ExecuteContract(ctx, userAccountKeyName, contractAddress, "{\"increment\":{}}"); err != nil {
-					return err
-				}
-				if _, err := srcChain.WaitForBlocks(1); err != nil {
-					return err
-				}
+		// run increment a bunch of times
+		for i := 0; i < 5; i++ {
+			if err := srcChain.ExecuteContract(ctx, userAccountKeyName, contractAddress, "{\"increment\":{}}"); err != nil {
+				return err
+			}
+			if _, err := srcChain.WaitForBlocks(1); err != nil {
+				return err
 			}
 		}
 	}
 
 	return nil
-
-	//return srcChain.WaitForBlocks(10)
 }
