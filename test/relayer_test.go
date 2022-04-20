f5e86dc76112d7e794f6c7d993b575980190121b
Mark Rushakoff <mark.rushakoff@gmail.com>
2022-04-20 10:43:59 -0400

    Move timestamp timeout test to relayertest

--- test/relayer_test.go
+++ test/relayer_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/strangelove-ventures/ibc-test-framework/ibc"
 	"github.com/strangelove-ventures/ibc-test-framework/relayertest"
-	"github.com/stretchr/testify/require"
 )
 
 // These tests are run by CI
@@ -13,34 +12,12 @@ import (
 func getTestChainFactory() ibc.ChainFactory {
 	return ibc.NewBuiltinChainFactory(
 		[]ibc.BuiltinChainFactoryEntry{
-			{Name: "gaia", Version: "v6.0.4", ChainID: "cosmoshub-1004", NumValidators: 4, NumFullNodes: 1},
-			{Name: "osmosis", Version: "v7.0.4", ChainID: "osmosis-1001", NumValidators: 4, NumFullNodes: 1},
+			{Name: "gaia", Version: "v6.0.4", ChainID: "cosmoshub-1004", NumValidators: 1, NumFullNodes: 1},
+			{Name: "osmosis", Version: "v7.0.4", ChainID: "osmosis-1001", NumValidators: 1, NumFullNodes: 1},
 		},
 	)
 }
 
-func TestRelayerByRelayerTest(t *testing.T) {
+func TestRelayer(t *testing.T) {
 	relayertest.TestRelayer(t, getTestChainFactory(), ibc.NewBuiltinRelayerFactory(ibc.CosmosRly))
-}
-
-// queued packet with no timeout should be relayed
-func TestNoTimeout(t *testing.T) {
-	relayerImplementation := ibc.CosmosRly
-
-	require.NoError(t, ibc.IBCTestCase{}.RelayPacketTestNoTimeout(t.Name(), getTestChainFactory(), relayerImplementation))
-}
-
-// queued packet with relative height timeout that expires should not be relayed
-func TestHeightTimeout(t *testing.T) {
-	relayerImplementation := ibc.CosmosRly
-
-	require.NoError(t, ibc.IBCTestCase{}.RelayPacketTestHeightTimeout(t.Name(), getTestChainFactory(), relayerImplementation))
-}
-
-// queued packet with relative timestamp timeout (ns) that expires should not be relayed
-func TestTimestampTimeout(t *testing.T) {
-	t.Skip() // TODO turn this back on once fixed in cosmos relayer
-	relayerImplementation := ibc.CosmosRly
-
-	require.NoError(t, ibc.IBCTestCase{}.RelayPacketTestTimestampTimeout(t.Name(), getTestChainFactory(), relayerImplementation))
 }
