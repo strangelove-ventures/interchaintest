ed2acbace027644a44104dfdcdd0e0f1fc3dd412
Andrew Gouin <andrew@gouin.io>
2022-04-27 18:56:55 -0600

    Better docker cleanup (#53)
    * Run cleanup before and after. Use latest images for CI and example matrix
    
    * Use fixed version for CI tests
    
    * use condense hostname for rpc and grpc hosts

--- test/relayer_test.go
+++ test/relayer_test.go
@@ -13,8 +13,8 @@ import (
 func getTestChainFactory() ibctest.ChainFactory {
 	return ibctest.NewBuiltinChainFactory(
 		[]ibctest.BuiltinChainFactoryEntry{
-			{Name: "gaia", Version: "v6.0.4", ChainID: "cosmoshub-1004", NumValidators: 1, NumFullNodes: 1},
-			{Name: "osmosis", Version: "v7.0.4", ChainID: "osmosis-1001", NumValidators: 1, NumFullNodes: 1},
+			{Name: "gaia", Version: "v7.0.1", ChainID: "cosmoshub-1004", NumValidators: 1, NumFullNodes: 1},
+			{Name: "osmosis", Version: "v7.2.0", ChainID: "osmosis-1001", NumValidators: 1, NumFullNodes: 1},
 		},
 	)
 }
