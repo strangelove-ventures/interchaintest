6e1c1c5eee42134e6a51e2fe9d12dd658f0d8d03
David Nix <hello@davidnix.io>
2022-05-02 16:32:09 -0600

    Package doc cleanup

--- log/doc.go
+++ log/doc.go
@@ -1,8 +1,11 @@
 // Package log is the preferred structured logging package to use throughout this codebase.
+//
 // It abstracts specific log implementations in case we want to change the concrete logger later.
 // E.g. zerolog, stdlib log, logrus, zap, etc.
 // Currently, the underlying logger is zerolog.
 //
-// Important: This package does not attempt to replace test log, i.e. t.Log and t.Logf. Test logs should still
+// This package does not concern itself with log performance.
+//
+// Important: This package does not replace testing's log, i.e. t.Log and t.Logf. Test logs should still
 // be used to inform the user of the test progress.
 package log
