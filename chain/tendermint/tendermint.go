3a4cf1b109a5c82c4bca7671642a1a436e03e155
David Nix <hello@davidnix.io>
2022-05-09 14:46:07 -0600

    Create event find helper

--- chain/tendermint/tendermint.go
+++ chain/tendermint/tendermint.go
@@ -6,9 +6,27 @@ import (
 	"fmt"
 
 	"github.com/davecgh/go-spew/spew"
+	abcitypes "github.com/tendermint/tendermint/abci/types"
 	coretypes "github.com/tendermint/tendermint/rpc/core/types"
 	"github.com/tendermint/tendermint/types"
 )
+
+// AttributeValue returns an event attribute value given the eventType and attribute key tuple.
+// In the event of duplicate types and keys, returns the first attribute value found.
+// If not found, ok is false.
+func AttributeValue(events []abcitypes.Event, eventType string, attrKey []byte) (found []byte, ok bool) {
+	for _, event := range events {
+		if event.Type != eventType {
+			continue
+		}
+		for _, attr := range event.Attributes {
+			if bytes.Equal(attr.Key, attrKey) {
+				return attr.Value, true
+			}
+		}
+	}
+	return nil, false
+}
 
 // PrettyPrintTxs pretty prints tendermint transactions and events
 func PrettyPrintTxs(ctx context.Context, txs types.Txs, getTx func(ctx context.Context, hash []byte, prove bool) (*coretypes.ResultTx, error)) (string, error) {
--- chain/tendermint/tendermint_test.go
+++ chain/tendermint/tendermint_test.go
@@ -10,6 +10,36 @@ import (
 	"github.com/tendermint/tendermint/types"
 )
 
+func TestAttributeValue(t *testing.T) {
+	events := []abcitypes.Event{
+		{Type: "1", Attributes: []abcitypes.EventAttribute{
+			{Key: []byte("ignore"), Value: []byte("should not see me")},
+			{Key: []byte("key1"), Value: []byte("found me 1")},
+		}},
+		{Type: "2", Attributes: []abcitypes.EventAttribute{
+			{Key: []byte("key2"), Value: []byte("found me 2")},
+			{Key: []byte("ignore"), Value: []byte("should not see me")},
+		}},
+	}
+
+	_, ok := AttributeValue(nil, "test", nil)
+	require.False(t, ok)
+
+	_, ok = AttributeValue(events, "key_not_there", []byte("ignored"))
+	require.False(t, ok)
+
+	_, ok = AttributeValue(events, "1", []byte("attribute not there"))
+	require.False(t, ok)
+
+	got, ok := AttributeValue(events, "1", []byte("key1"))
+	require.True(t, ok)
+	require.Equal(t, "found me 1", string(got))
+
+	got, ok = AttributeValue(events, "2", []byte("key2"))
+	require.True(t, ok)
+	require.Equal(t, "found me 2", string(got))
+}
+
 func TestPrettyPrintTxs(t *testing.T) {
 	ctx := context.Background()
 
