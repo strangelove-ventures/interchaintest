135a975f486398a906e20900379ace34ebf2fb28
David Nix <hello@davidnix.io>
2022-05-18 16:33:58 -0600

    doc clarification

--- ibc/doc.go
+++ ibc/doc.go
@@ -1,8 +1,7 @@
-// Package ibc provides reusable, chain-agnostic types for to the IBC spec.
+// Package ibc provides chain and relayer agnostic types and interfaces for the IBC spec.
 // It attempts to expose the domain model of IBC.
 //
 // The official spec documentation can be found at https://github.com/cosmos/ibc/tree/master/spec.
 //
-// This package provides interfaces any new chains and relayers must implement.
 // Currently, the interfaces may be biased towards chains built with the cosmos sdk and the cosmos/relayer.
 package ibc
