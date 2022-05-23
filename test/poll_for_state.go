1c97d40736ccc1961fcaef8649de1e9536b0a3a3
David Nix <hello@davidnix.io>
2022-05-23 16:20:23 -0600

    Add doc comment

--- test/poll_for_state.go
+++ test/poll_for_state.go
@@ -7,11 +7,16 @@ import (
 	"github.com/strangelove-ventures/ibctest/ibc"
 )
 
+// ChainAcker is a chain that can get its acknowledgements at a specified height
 type ChainAcker interface {
 	ChainHeighter
 	Acknowledgements(ctx context.Context, height uint64) ([]ibc.PacketAcknowledgement, error)
 }
 
+// PollForAck attempts to find an acknowledgement containing a packet equal to the packet argument.
+// Polling starts at startHeight and continues until maxHeight. It is safe to call this function even if
+// the chain has yet to produce blocks for the target min/max height range. Polling delays until heights exist
+// on the chain. If no acknowledgement found, returns a not-found error.
 func PollForAck(ctx context.Context, chain ChainAcker, startHeight, maxHeight uint64, packet ibc.Packet) (zero ibc.PacketAcknowledgement, _ error) {
 	if maxHeight < startHeight {
 		panic("maxHeight must be greater than or equal to startHeight")
