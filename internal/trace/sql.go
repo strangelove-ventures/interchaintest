e83f038f7895be93e474131d491e2535297b5983
David Nix <hello@davidnix.io>
2022-05-27 15:46:17 -0600

    fix: ping db with context

--- internal/trace/sql.go
+++ internal/trace/sql.go
@@ -8,13 +8,16 @@ import (
 	_ "modernc.org/sqlite"
 )
 
+// ConnectDB connects to the sqlite database at filePath with max connections set to maxConns.
+// Pings database once to ensure connection.
+// Pass :memory: as filePath for in-memory database.
 func ConnectDB(ctx context.Context, filePath string, maxConns int) (*sql.DB, error) {
 	db, err := sql.Open("sqlite", filePath)
 	if err != nil {
 		return nil, fmt.Errorf("open db %s: %w", filePath, err)
 	}
 	db.SetMaxOpenConns(maxConns)
-	err = db.Ping()
+	err = db.PingContext(ctx)
 	if err != nil {
 		_ = db.Close()
 		return nil, fmt.Errorf("ping db %s: %w", filePath, err)
