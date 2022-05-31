69c372394396028296b05663f8a560efe90eefbf
David Nix <DavidNix@users.noreply.github.com>
2022-05-31 09:55:12 -0600

    feat: Add trace package to track chains, blocks, and txs (#113)

---
+++ internal/blockdb/sql_test.go
@@ -0,0 +1,37 @@
+package blockdb
+
+import (
+	"context"
+	"database/sql"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func emptyDB() *sql.DB {
+	db, err := ConnectDB(context.Background(), ":memory:", 3)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func migratedDB() *sql.DB {
+	db := emptyDB()
+	if err := Migrate(db); err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func TestConnectDB(t *testing.T) {
+	f, err := os.CreateTemp("", t.Name())
+	require.NoError(t, err)
+	defer f.Close()
+	defer os.RemoveAll(f.Name())
+
+	db, err := ConnectDB(context.Background(), f.Name(), 10)
+	require.NoError(t, err)
+	require.NoError(t, db.Close())
+}
