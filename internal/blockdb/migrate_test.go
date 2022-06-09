c3a3fbd186106585d92ef2947fb6e01e83668b62
David Nix <hello@davidnix.io>
2022-06-09 10:05:46 -0600

    Add chain_type col to chain table

--- internal/blockdb/migrate_test.go
+++ internal/blockdb/migrate_test.go
@@ -7,37 +7,51 @@ import (
 )
 
 func TestMigrate(t *testing.T) {
-	db := emptyDB()
-	defer db.Close()
+	t.Parallel()
 
-	const gitSha = "abc123"
-	err := Migrate(db, gitSha)
-	require.NoError(t, err)
+	t.Run("happy path", func(t *testing.T) {
+		db := emptyDB()
+		defer db.Close()
 
-	// idempotent
-	err = Migrate(db, gitSha)
-	require.NoError(t, err)
+		const gitSha = "abc123"
+		err := Migrate(db, gitSha)
+		require.NoError(t, err)
 
-	row := db.QueryRow(`select count(*) from schema_version`)
-	var count int
-	err = row.Scan(&count)
+		// idempotent
+		err = Migrate(db, gitSha)
+		require.NoError(t, err)
 
-	require.NoError(t, err)
-	require.Equal(t, 1, count)
+		row := db.QueryRow(`select count(*) from schema_version`)
+		var count int
+		err = row.Scan(&count)
 
-	err = Migrate(db, "new-sha")
-	require.NoError(t, err)
+		require.NoError(t, err)
+		require.Equal(t, 1, count)
 
-	row = db.QueryRow(`select count(*) from schema_version`)
-	err = row.Scan(&count)
+		err = Migrate(db, "new-sha")
+		require.NoError(t, err)
 
-	require.NoError(t, err)
-	require.Equal(t, 2, count)
+		row = db.QueryRow(`select count(*) from schema_version`)
+		err = row.Scan(&count)
 
-	row = db.QueryRow(`select git_sha from schema_version order by id desc limit 1`)
-	var gotSha string
-	err = row.Scan(&gotSha)
+		require.NoError(t, err)
+		require.Equal(t, 2, count)
 
-	require.NoError(t, err)
-	require.Equal(t, "new-sha", gotSha)
+		row = db.QueryRow(`select git_sha from schema_version order by id desc limit 1`)
+		var gotSha string
+		err = row.Scan(&gotSha)
+
+		require.NoError(t, err)
+		require.Equal(t, "new-sha", gotSha)
+	})
+
+	t.Run("idempotent", func(t *testing.T) {
+		// Start with an existing schema.
+		db := migratedDB()
+		defer db.Close()
+
+		// Migrate against the existing schema.
+		err := Migrate(db, "abc123")
+		require.NoError(t, err)
+	})
 }
