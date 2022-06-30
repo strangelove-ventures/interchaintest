f35ea95d3e9d227d6978e5e8344d2be610b99a97
Cian Hatton <cianhatton@gmail.com>
2022-06-30 18:08:33 +0100

    chore: reverted default values

--- chainfactory.go
+++ chainfactory.go
@@ -85,8 +85,8 @@ func (f *BuiltinChainFactory) Chains(testName string) ([]ibc.Chain, error) {
 }
 
 const (
-	defaultNumValidators = 1
-	defaultNumFullNodes  = 0
+	defaultNumValidators = 2
+	defaultNumFullNodes  = 1
 )
 
 func buildChain(log *zap.Logger, testName string, cfg ibc.ChainConfig, numValidators, numFullNodes *int) (ibc.Chain, error) {
