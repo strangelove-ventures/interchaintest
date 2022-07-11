bdeaa69c90aaa8d07b034e6eb944137252a99101
Mark Rushakoff <mark.rushakoff@gmail.com>
2022-07-11 17:05:08 -0400

    Use docker to retrieve genesis hashes
    This is one step towards #200, using ephemeral containers to read the
    genesis file content from the docker volume instead of reading from the
    host disk.
    
    Note that as of this commit, the docker volume is still a host mount,
    but it should be seamless to switch this usage to a volume once
    everything else for #200 is in place.

---
+++ internal/dockerutil/fileretriever_test.go
@@ -0,0 +1,63 @@
+package dockerutil_test
+
+import (
+	"context"
+	"testing"
+
+	volumetypes "github.com/docker/docker/api/types/volume"
+	"github.com/strangelove-ventures/ibctest"
+	"github.com/strangelove-ventures/ibctest/internal/dockerutil"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+)
+
+func TestFileRetriever(t *testing.T) {
+	t.Parallel()
+
+	cli, network := ibctest.DockerSetup(t)
+
+	ctx := context.Background()
+	v, err := cli.VolumeCreate(ctx, volumetypes.VolumeCreateBody{
+		Labels: map[string]string{dockerutil.CleanupLabel: t.Name()},
+	})
+	require.NoError(t, err)
+
+	img := dockerutil.NewImage(
+		zaptest.NewLogger(t),
+		cli,
+		network,
+		t.Name(),
+		"busybox", "stable",
+	)
+
+	_, _, err = img.Run(
+		ctx,
+		[]string{"sh", "-c", "printf 'hello world' > /mnt/test/hello.txt"},
+		dockerutil.ContainerOptions{
+			Binds: []string{v.Name + ":/mnt/test"},
+		},
+	)
+	require.NoError(t, err)
+	_, _, err = img.Run(
+		ctx,
+		[]string{"sh", "-c", "mkdir -p /mnt/test/foo/bar/ && printf 'test' > /mnt/test/foo/bar/baz.txt"},
+		dockerutil.ContainerOptions{
+			Binds: []string{v.Name + ":/mnt/test"},
+		},
+	)
+	require.NoError(t, err)
+
+	fr := dockerutil.NewFileRetriever(zaptest.NewLogger(t), cli, t.Name())
+
+	t.Run("top-level file", func(t *testing.T) {
+		b, err := fr.SingleFileContent(ctx, v.Name, "hello.txt")
+		require.NoError(t, err)
+		require.Equal(t, string(b), "hello world")
+	})
+
+	t.Run("nested file", func(t *testing.T) {
+		b, err := fr.SingleFileContent(ctx, v.Name, "foo/bar/baz.txt")
+		require.NoError(t, err)
+		require.Equal(t, string(b), "test")
+	})
+}
