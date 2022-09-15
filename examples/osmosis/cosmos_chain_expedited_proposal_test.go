9942cb1a4f190c247bec5dda0b17bcbedb4dfdd6
Andrew Gouin <andrew@gouin.io>
2022-09-15 14:18:51 -0600

    [v3] expedited proposals test (#281)
    * Add osmosis module with expedited proposal test
    
    * Merge PR #282: Add gamm pool test

---
+++ examples/osmosis/cosmos_chain_expedited_proposal_test.go
@@ -0,0 +1,143 @@
+package osmosis_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/icza/dyno"
+	ibctest "github.com/strangelove-ventures/ibctest/v3"
+	"github.com/strangelove-ventures/ibctest/v3/chain/cosmos"
+	"github.com/strangelove-ventures/ibctest/v3/examples/osmosis"
+	"github.com/strangelove-ventures/ibctest/v3/ibc"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+)
+
+func TestOsmosisExpeditedProposal(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	t.Parallel()
+
+	const (
+		votingPeriod          = "1h"
+		expeditedVotingPeriod = "10s"
+		maxDepositPeriod      = "10s"
+		minDeposit            = "10000000"
+		minExpeditedDeposit   = "50000000"
+	)
+	chainSpec := &ibctest.ChainSpec{
+		Name:      "osmosis",
+		ChainName: "osmosis",
+		Version:   "main",
+		ChainConfig: ibc.ChainConfig{
+			ChainID:        "osmosis-1001", // hardcoded handling in osmosis binary for osmosis-1, so need to override to something different.
+			ModifyGenesis:  modifyGenesisExpeditedProposals(votingPeriod, expeditedVotingPeriod, minDeposit, minExpeditedDeposit, maxDepositPeriod),
+			EncodingConfig: osmosis.OsmosisEncoding(),
+		},
+	}
+
+	cf := ibctest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*ibctest.ChainSpec{chainSpec})
+
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+
+	chain := chains[0].(*cosmos.CosmosChain)
+
+	ic := ibctest.NewInterchain().
+		AddChain(chain)
+
+	ctx := context.Background()
+	client, network := ibctest.DockerSetup(t)
+
+	require.NoError(t, ic.Build(ctx, nil, ibctest.InterchainBuildOptions{
+		TestName:          t.Name(),
+		Client:            client,
+		NetworkID:         network,
+		BlockDatabaseFile: ibctest.DefaultBlockDatabaseFilepath(),
+		SkipPathCreation:  true,
+	}))
+	t.Cleanup(func() {
+		_ = ic.Close()
+	})
+
+	const userFunds = int64(10_000_000_000)
+	users := ibctest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, chain)
+	chainUser := users[0]
+
+	// submit non-expedited proposal first. will make assertions last, since this one should still
+	// be in voting period after the expedited proposal passes.
+	nonExpeditedProposalTx, err := chain.TextProposal(ctx, chainUser.KeyName, cosmos.TextProposal{
+		Deposit:     minDeposit + chain.Config().Denom,
+		Title:       "Chain Proposal 1",
+		Description: "Non-expedited chain governance proposal",
+		Expedited:   false,
+	})
+	require.NoError(t, err, "error submitting text proposal tx")
+
+	expeditedProposalTx, err := chain.TextProposal(ctx, chainUser.KeyName, cosmos.TextProposal{
+		Deposit:     minExpeditedDeposit + chain.Config().Denom,
+		Title:       "Chain Proposal 2",
+		Description: "Expedited chain governance proposal",
+		Expedited:   true,
+	})
+	require.NoError(t, err, "error submitting text proposal tx")
+
+	postProposalHeight, err := chain.Height(ctx)
+	require.NoError(t, err, "failed to fetch chain height after submitting proposals")
+
+	err = chain.VoteOnProposalAllValidators(ctx, nonExpeditedProposalTx.ProposalID, cosmos.ProposalVoteYes)
+	require.NoError(t, err, "failed to submit votes")
+
+	err = chain.VoteOnProposalAllValidators(ctx, expeditedProposalTx.ProposalID, cosmos.ProposalVoteYes)
+	require.NoError(t, err, "failed to submit votes")
+
+	_, err = cosmos.PollForProposalStatus(ctx, chain, postProposalHeight, postProposalHeight+10, expeditedProposalTx.ProposalID, cosmos.ProposalStatusPassed)
+	require.NoError(t, err, "expedited proposal did not change to passed status in expected timeframe")
+
+	nonExpeditedProposal, err := chain.QueryProposal(ctx, nonExpeditedProposalTx.ProposalID)
+	require.NoError(t, err, "failed to query non-expedited proposal status")
+	require.Equal(t, cosmos.ProposalStatusVotingPeriod, nonExpeditedProposal.Status, "non-expedited proposal is not in voting period")
+}
+
+func modifyGenesisExpeditedProposals(
+	votingPeriod, expeditedVotingPeriod string,
+	minDepositAmount, minExpeditedDepositAmount string,
+	maxDepositPeriod string,
+) func(ibc.ChainConfig, []byte) ([]byte, error) {
+	return func(chainConfig ibc.ChainConfig, genbz []byte) ([]byte, error) {
+		g := make(map[string]interface{})
+		if err := json.Unmarshal(genbz, &g); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal genesis file: %w", err)
+		}
+		if err := dyno.Set(g, votingPeriod, "app_state", "gov", "voting_params", "voting_period"); err != nil {
+			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+		}
+		if err := dyno.Set(g, expeditedVotingPeriod, "app_state", "gov", "voting_params", "expedited_voting_period"); err != nil {
+			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+		}
+		if err := dyno.Set(g, maxDepositPeriod, "app_state", "gov", "deposit_params", "max_deposit_period"); err != nil {
+			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+		}
+		if err := dyno.Set(g, chainConfig.Denom, "app_state", "gov", "deposit_params", "min_deposit", 0, "denom"); err != nil {
+			return nil, fmt.Errorf("failed to set min deposit denom in genesis json: %w", err)
+		}
+		if err := dyno.Set(g, minDepositAmount, "app_state", "gov", "deposit_params", "min_deposit", 0, "amount"); err != nil {
+			return nil, fmt.Errorf("failed to set min deposit amount in genesis json: %w", err)
+		}
+		if err := dyno.Set(g, chainConfig.Denom, "app_state", "gov", "deposit_params", "min_expedited_deposit", 0, "denom"); err != nil {
+			return nil, fmt.Errorf("failed to set min deposit denom in genesis json: %w", err)
+		}
+		if err := dyno.Set(g, minExpeditedDepositAmount, "app_state", "gov", "deposit_params", "min_expedited_deposit", 0, "amount"); err != nil {
+			return nil, fmt.Errorf("failed to set min expedited deposit amount in genesis json: %w", err)
+		}
+		out, err := json.Marshal(g)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal genesis bytes to json: %w", err)
+		}
+		return out, nil
+	}
+}
---
+++ examples/osmosis/osmosis.go
@@ -0,0 +1,17 @@
+package osmosis
+
+import (
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
+	balancertypes "github.com/osmosis-labs/osmosis/v12/x/gamm/pool-models/balancer"
+	gammtypes "github.com/osmosis-labs/osmosis/v12/x/gamm/types"
+	"github.com/strangelove-ventures/ibctest/v3/chain/cosmos"
+)
+
+func OsmosisEncoding() *simappparams.EncodingConfig {
+	cfg := cosmos.DefaultEncoding()
+
+	balancertypes.RegisterInterfaces(cfg.InterfaceRegistry)
+	gammtypes.RegisterInterfaces(cfg.InterfaceRegistry)
+
+	return &cfg
+}
