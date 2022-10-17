bc0a2035162d0cb003528319f3cc2adb64fc9cca
David Nix <DavidNix@users.noreply.github.com>
2022-10-17 15:22:06 -0600

    feat: Add cosmos message poller (#325)

--- chain/cosmos/poll.go
+++ chain/cosmos/poll.go
@@ -2,15 +2,17 @@ package cosmos
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/strangelove-ventures/ibctest/v3/test"
 )
 
 // PollForProposalStatus attempts to find a proposal with matching ID and status.
 func PollForProposalStatus(ctx context.Context, chain *CosmosChain, startHeight, maxHeight uint64, proposalID string, status string) (ProposalResponse, error) {
 	var zero ProposalResponse
-	doPoll := func(ctx context.Context, height uint64) (any, error) {
+	doPoll := func(ctx context.Context, height uint64) (ProposalResponse, error) {
 		p, err := chain.QueryProposal(ctx, proposalID)
 		if err != nil {
 			return zero, err
@@ -20,10 +22,39 @@ func PollForProposalStatus(ctx context.Context, chain *CosmosChain, startHeight,
 		}
 		return *p, nil
 	}
-	bp := test.BlockPoller{CurrentHeight: chain.Height, PollFunc: doPoll}
-	p, err := bp.DoPoll(ctx, startHeight, maxHeight)
-	if err != nil {
-		return zero, err
+	bp := test.BlockPoller[ProposalResponse]{CurrentHeight: chain.Height, PollFunc: doPoll}
+	return bp.DoPoll(ctx, startHeight, maxHeight)
+}
+
+// PollForMessage searches every transaction for a message. Must pass a coded registry capable of decoding the cosmos transaction.
+// fn is optional. Return true from the fn to stop polling and return the found message. If fn is nil, returns the first message to match type T.
+func PollForMessage[T any](ctx context.Context, chain *CosmosChain, registry codectypes.InterfaceRegistry, startHeight, maxHeight uint64, fn func(found T) bool) (T, error) {
+	var zero T
+	if fn == nil {
+		fn = func(T) bool { return true }
 	}
-	return p.(ProposalResponse), nil
+	doPoll := func(ctx context.Context, height uint64) (T, error) {
+		h := int64(height)
+		block, err := chain.getFullNode().Client.Block(ctx, &h)
+		if err != nil {
+			return zero, err
+		}
+		for _, tx := range block.Block.Txs {
+			sdkTx, err := decodeTX(registry, tx)
+			if err != nil {
+				return zero, err
+			}
+			for _, msg := range sdkTx.GetMsgs() {
+				if found, ok := msg.(T); ok {
+					if fn(found) {
+						return found, nil
+					}
+				}
+			}
+		}
+		return zero, errors.New("not found")
+	}
+
+	bp := test.BlockPoller[T]{CurrentHeight: chain.Height, PollFunc: doPoll}
+	return bp.DoPoll(ctx, startHeight, maxHeight)
 }
---
+++ examples/cosmos/versions_test.go
@@ -0,0 +1,6 @@
+package cosmos_test
+
+const (
+	gaiaVersion    = "v7.1.0"
+	osmosisVersion = "v12.2.0"
+)
