a1e091ee179dce4c9a44e00ddd8b310403131051
Cian Hatton <cianhatton@gmail.com>
2023-02-14 12:36:22 +0000

    undid import change

--- conformance/relayersetup.go
+++ conformance/relayersetup.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 	"testing"
 
+	conntypes "github.com/cosmos/ibc-go/v6/modules/core/03-connection/types"
 	interchaintest "github.com/strangelove-ventures/interchaintest/v6"
 	"github.com/strangelove-ventures/interchaintest/v6/ibc"
 	"github.com/strangelove-ventures/interchaintest/v6/testreporter"
 	"github.com/strangelove-ventures/interchaintest/v6/testutil"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/sync/errgroup"
-
-	conntypes "github.com/cosmos/ibc-go/v6/modules/core/03-connection/types"
 )
 
 // TestRelayerSetup contains a series of subtests that configure a relayer step-by-step.
