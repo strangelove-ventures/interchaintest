b74ff72ffa82c4a83e38b27d9a702349501f0215
colin axnér <25233464+colin-axner@users.noreply.github.com>
2023-03-23 22:21:55 +0100

    fix: remove race condition on start container retries (#415)
    * fix: increase retry to 15 seconds to avoid retrying a command exec
    
    * chore: increase log tail, simplify start container logic
    
    * chore: cleanup, revert change, add in-code comment
    
    * chore: remove reference to retry

--- internal/dockerutil/startcontainer.go
+++ internal/dockerutil/startcontainer.go
@@ -2,37 +2,25 @@ package dockerutil
 
 import (
 	"context"
-	"errors"
 	"time"
 
-	"github.com/avast/retry-go/v4"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
 // StartContainer attempts to start the container with the given ID.
-// If the request times out, it retries a certain number of times before failing.
-// Any other failure modes stop immediately.
 func StartContainer(ctx context.Context, cli *client.Client, id string) error {
-	return retry.Do(
-		func() error {
-			retryCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
-			defer cancel()
+	// add a deadline for the request if the calling context does not provide one
+	if _, hasDeadline := ctx.Deadline(); !hasDeadline {
+		var cancel func()
+		ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+		defer cancel()
+	}
 
-			err := cli.ContainerStart(retryCtx, id, types.ContainerStartOptions{})
-			if err != nil {
-				// One special case: retryCtx timed out and the outer ctx didn't.
-				if errors.Is(err, context.DeadlineExceeded) && ctx.Err() == nil {
-					return err
-				}
+	err := cli.ContainerStart(ctx, id, types.ContainerStartOptions{})
+	if err != nil {
+		return err
+	}
 
-				// Otherwise, assume the error cannot be retried.
-				return retry.Unrecoverable(err)
-			}
-
-			return nil
-		},
-		retry.Context(ctx),
-		retry.DelayType(retry.FixedDelay),
-	)
+	return nil
 }
