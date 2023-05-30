fb80e277cfc7c4f58974816680413f4262f0ae61
Jim Fasarakis-Hilliard <d.f.hilliard@gmail.com>
2023-05-30 18:44:24 +0300

    Allow extra startup flags for hermes. (#583)
    Co-authored-by: Steve <1848680+misko9@users.noreply.github.com>
    (cherry picked from commit e27ebbd69e93771798065899565b6879f350d1a2)

--- relayer/hermes/hermes_relayer.go
+++ relayer/hermes/hermes_relayer.go
@@ -62,6 +62,12 @@ type pathChainConfig struct {
 // NewHermesRelayer returns a new hermes relayer.
 func NewHermesRelayer(log *zap.Logger, testName string, cli *client.Client, networkID string, options ...relayer.RelayerOption) *Relayer {
 	c := commander{log: log}
+	for _, opt := range options {
+		switch o := opt.(type) {
+		case relayer.RelayerOptionExtraStartFlags:
+			c.extraStartFlags = o.Flags
+		}
+	}
 	options = append(options, relayer.HomeDir(hermesHome))
 	dr, err := relayer.NewDockerRelayer(context.TODO(), log, testName, cli, networkID, c, options...)
 	if err != nil {
