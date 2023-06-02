46ca0f3221f9d6f5fa412831f6c0ff274a6be8f0
Dan Kanefsky <daniel.kanefsky@gmail.com>
2023-06-02 09:37:18 -0700

    check gov mod version

--- chain/cosmos/genesis.go
+++ chain/cosmos/genesis.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	govv1types "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	"github.com/icza/dyno"
 	"github.com/strangelove-ventures/interchaintest/v7/ibc"
 )
@@ -14,14 +15,32 @@ func ModifyGenesisProposalTime(votingPeriod string, maxDepositPeriod string) fun
 		if err := json.Unmarshal(genbz, &g); err != nil {
 			return nil, fmt.Errorf("failed to unmarshal genesis file: %w", err)
 		}
-		if err := dyno.Set(g, votingPeriod, "app_state", "gov", "voting_params", "voting_period"); err != nil {
-			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+
+		govGsV1 := govv1types.GenesisState{}
+		if err := json.Unmarshal(genbz, &govGsV1); err != nil {
+			return nil, fmt.Errorf("failed to unmarshall genesis file: %w", err)
 		}
-		if err := dyno.Set(g, maxDepositPeriod, "app_state", "gov", "deposit_params", "max_deposit_period"); err != nil {
-			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
-		}
-		if err := dyno.Set(g, chainConfig.Denom, "app_state", "gov", "deposit_params", "min_deposit", 0, "denom"); err != nil {
-			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+		if govGsV1.Empty() {
+			// if empty, the chain's gov module is on v1beta1, else it's on v1
+			if err := dyno.Set(g, votingPeriod, "app_state", "gov", "voting_params", "voting_period"); err != nil {
+				return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+			}
+			if err := dyno.Set(g, maxDepositPeriod, "app_state", "gov", "deposit_params", "max_deposit_period"); err != nil {
+				return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+			}
+			if err := dyno.Set(g, chainConfig.Denom, "app_state", "gov", "deposit_params", "min_deposit", 0, "denom"); err != nil {
+				return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+			}
+		} else {
+			if err := dyno.Set(g, votingPeriod, "app_state", "gov", "params", "voting_period"); err != nil {
+				return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+			}
+			if err := dyno.Set(g, maxDepositPeriod, "app_state", "gov", "params", "max_deposit_period"); err != nil {
+				return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+			}
+			if err := dyno.Set(g, chainConfig.Denom, "app_state", "gov", "params", "min_deposit", 0, "denom"); err != nil {
+				return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+			}
 		}
 		out, err := json.Marshal(g)
 		if err != nil {
