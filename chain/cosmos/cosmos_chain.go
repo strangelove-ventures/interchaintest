c0989e954796afae888d79f35a3a238e8f52ee92
Steve Miskovetz <steve.miskovetz@gmail.com>
2023-06-05 13:55:17 -0600

    Add submit proposal

--- chain/cosmos/cosmos_chain.go
+++ chain/cosmos/cosmos_chain.go
@@ -388,6 +388,15 @@ func (c *CosmosChain) PushNewWasmClientProposal(ctx context.Context, keyName str
 	return tx, codeHash, err
 }
 
+// SubmitProposal submits a gov v1 proposal to the chain.
+func (c *CosmosChain) SubmitProposal(ctx context.Context, keyName string, prop TxProposalv1) (tx TxProposal, _ error) {
+	txHash, err := c.getFullNode().SubmitProposal(ctx, keyName, prop)
+	if err != nil {
+		return tx, fmt.Errorf("failed to submit gov v1 proposal: %w", err)
+	}
+	return c.txProposal(txHash)
+}
+
 // UpgradeProposal submits a software-upgrade governance proposal to the chain.
 func (c *CosmosChain) UpgradeProposal(ctx context.Context, keyName string, prop SoftwareUpgradeProposal) (tx TxProposal, _ error) {
 	txHash, err := c.getFullNode().UpgradeProposal(ctx, keyName, prop)
