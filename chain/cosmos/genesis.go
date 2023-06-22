99195f217ab7537d6640b01d92907589604b15e3
Dan Kanefsky <daniel.kanefsky@gmail.com>
2023-06-22 15:14:54 -0700

    backport modifyGenesis cmd from #565

--- chain/cosmos/genesis.go
+++ chain/cosmos/genesis.go
@@ -3,26 +3,42 @@ package cosmos
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/icza/dyno"
 	"github.com/strangelove-ventures/interchaintest/v3/ibc"
 )
 
-func ModifyGenesisProposalTime(votingPeriod string, maxDepositPeriod string) func(ibc.ChainConfig, []byte) ([]byte, error) {
+type GenesisKV struct {
+	Key   string      `json:"key"`
+	Value interface{} `json:"value"`
+}
+
+func ModifyGenesis(genesisKV []GenesisKV) func(ibc.ChainConfig, []byte) ([]byte, error) {
 	return func(chainConfig ibc.ChainConfig, genbz []byte) ([]byte, error) {
 		g := make(map[string]interface{})
 		if err := json.Unmarshal(genbz, &g); err != nil {
 			return nil, fmt.Errorf("failed to unmarshal genesis file: %w", err)
 		}
-		if err := dyno.Set(g, votingPeriod, "app_state", "gov", "voting_params", "voting_period"); err != nil {
-			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+
+		for idx, values := range genesisKV {
+			splitPath := strings.Split(values.Key, ".")
+
+			path := make([]interface{}, len(splitPath))
+			for i, component := range splitPath {
+				if v, err := strconv.Atoi(component); err == nil {
+					path[i] = v
+				} else {
+					path[i] = component
+				}
+			}
+
+			if err := dyno.Set(g, values.Value, path...); err != nil {
+				return nil, fmt.Errorf("failed to set value (index:%d) in genesis json: %w", idx, err)
+			}
 		}
-		if err := dyno.Set(g, maxDepositPeriod, "app_state", "gov", "deposit_params", "max_deposit_period"); err != nil {
-			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
-		}
-		if err := dyno.Set(g, chainConfig.Denom, "app_state", "gov", "deposit_params", "min_deposit", 0, "denom"); err != nil {
-			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
-		}
+
 		out, err := json.Marshal(g)
 		if err != nil {
 			return nil, fmt.Errorf("failed to marshal genesis bytes to json: %w", err)
