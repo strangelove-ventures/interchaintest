5af2aa189cb65c63b6b2c730016e29570fbc31ac
Giuseppe Valente <giuseppe@polymerlabs.org>
2023-06-27 13:44:44 +0200

    feat: add env to add registry auth

--- chain/cosmos/cosmos_chain.go
+++ chain/cosmos/cosmos_chain.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"context"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -63,7 +65,8 @@ func NewCosmosHeighlinerChainConfig(name string,
 	gasPrices string,
 	gasAdjustment float64,
 	trustingPeriod string,
-	noHostMount bool) ibc.ChainConfig {
+	noHostMount bool,
+) ibc.ChainConfig {
 	return ibc.ChainConfig{
 		Type:           "cosmos",
 		Name:           name,
@@ -490,7 +493,6 @@ func (c *CosmosChain) GetBalance(ctx context.Context, address string, denom stri
 
 	queryClient := bankTypes.NewQueryClient(conn)
 	res, err := queryClient.Balance(ctx, params)
-
 	if err != nil {
 		return 0, err
 	}
@@ -510,7 +512,6 @@ func (c *CosmosChain) AllBalances(ctx context.Context, address string) (types.Co
 
 	queryClient := bankTypes.NewQueryClient(conn)
 	res, err := queryClient.AllBalances(ctx, &params)
-
 	if err != nil {
 		return nil, err
 	}
@@ -555,11 +556,23 @@ func (c *CosmosChain) UpgradeVersion(ctx context.Context, cli *client.Client, co
 }
 
 func (c *CosmosChain) pullImages(ctx context.Context, cli *client.Client) {
+	user := os.Getenv("DOCKER_REGISTRY_USER")
+	pass := os.Getenv("DOCKER_REGISTRY_PASS")
+	var registryAuth string
+	if user != "" && pass != "" {
+		jsonBytes, _ := json.Marshal(map[string]string{
+			"username": user,
+			"password": pass,
+		})
+		registryAuth = base64.StdEncoding.EncodeToString(jsonBytes)
+	}
 	for _, image := range c.Config().Images {
 		rc, err := cli.ImagePull(
 			ctx,
 			image.Repository+":"+image.Version,
-			dockertypes.ImagePullOptions{},
+			dockertypes.ImagePullOptions{
+				RegistryAuth: registryAuth,
+			},
 		)
 		if err != nil {
 			c.log.Error("Failed to pull image",
@@ -818,7 +831,7 @@ func (c *CosmosChain) Start(testName string, ctx context.Context, additionalGene
 			zap.String("chain", exportGenesisChain),
 			zap.String("path", exportGenesis),
 		)
-		_ = os.WriteFile(exportGenesis, genbz, 0600)
+		_ = os.WriteFile(exportGenesis, genbz, 0o600)
 	}
 
 	chainNodes := c.Nodes()
