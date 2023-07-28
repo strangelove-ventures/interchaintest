e85b6c555528d64e1196f28d041810f97ca645a3
Reece Williams <31943163+Reecepbcups@users.noreply.github.com>
2023-07-28 13:18:29 -0500

    Better error for root cli execute
    Co-authored-by: Dan Kanefsky <56059752+boojamya@users.noreply.github.com>

--- local-interchain/cmd/local-ic/main.go
+++ local-interchain/cmd/local-ic/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		fmt.Fprintf(os.Stderr, "error while executing your CLI. Err: %v\n", err)
 		os.Exit(1)
 	}
 }
