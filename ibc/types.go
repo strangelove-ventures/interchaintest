5fdb1e8a62e5e8b53e4ee918915225745bf7065c
Nikhil Vasan <97126437+nivasan1@users.noreply.github.com>
2023-08-10 16:35:57 -0700

    feat(cosmos): Additional Helpers (#687)
    * injective / evm fixes
    
    * chain-id flag conditional

--- ibc/types.go
+++ ibc/types.go
@@ -7,6 +7,7 @@ import (
 	"cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/types/module/testutil"
 	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/03-connection/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // ChainConfig defines the chain parameters requires to run an interchaintest testnet for a chain.
@@ -41,12 +42,16 @@ type ChainConfig struct {
 	PreGenesis func(ChainConfig) error
 	// When provided, genesis file contents will be altered before sharing for genesis.
 	ModifyGenesis func(ChainConfig, []byte) ([]byte, error)
+	// Modify genesis-amounts
+	ModifyGenesisAmounts func() (sdk.Coin, sdk.Coin)
 	// Override config parameters for files at filepath.
 	ConfigFileOverrides map[string]any
 	// Non-nil will override the encoding config, used for cosmos chains only.
 	EncodingConfig *testutil.TestEncodingConfig
 	// Required when the chain uses the new sub commands for genesis (https://github.com/cosmos/cosmos-sdk/pull/14149)
 	UsingNewGenesisCommand bool `yaml:"using-new-genesis-command"`
+	// Required when the chain requires the chain-id field to be populated for certain commands
+	UsingChainIDFlagCLI bool `yaml:"using-chain-id-flag-cli"`
 	// Configuration describing additional sidecar processes.
 	SidecarConfigs []SidecarConfig
 }
