fbead95cfa0df5666f291fba977c57d5656a0f3d
vimystic <122659254+vimystic@users.noreply.github.com>
2023-08-14 00:17:01 -0600

    use apt alias for ics_test.go

--- examples/ibc/ics_test.go
+++ examples/ibc/ics_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	ibctest "github.com/strangelove-ventures/interchaintest/v7"
+	interchaintest "github.com/strangelove-ventures/interchaintest/v7"
 	"github.com/strangelove-ventures/interchaintest/v7/ibc"
 	"github.com/strangelove-ventures/interchaintest/v7/relayer"
 	"github.com/strangelove-ventures/interchaintest/v7/relayer/rly"
@@ -27,9 +27,9 @@ func TestICS(t *testing.T) {
 	ctx := context.Background()
 
 	// Chain Factory
-	cf := ibctest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*ibctest.ChainSpec{
-		{Name: "gaia", Version: "v10.0.1", ChainConfig: ibc.ChainConfig{GasAdjustment: 1.5}},
-		{Name: "ics-consumer", Version: "v2.0.0"},
+	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
+		{Name: "gaia", Version: "v10.0.2-lsm-debug-v7"},
+		{Name: "stride", Version: "v13.1.0"},
 	})
 
 	chains, err := cf.Chains(t.Name())
@@ -37,8 +37,8 @@ func TestICS(t *testing.T) {
 	provider, consumer := chains[0], chains[1]
 
 	// Relayer Factory
-	client, network := ibctest.DockerSetup(t)
-	r := ibctest.NewBuiltinRelayerFactory(
+	client, network := interchaintest.DockerSetup(t)
+	r := interchaintest.NewBuiltinRelayerFactory(
 		ibc.CosmosRly,
 		zaptest.NewLogger(t),
 		relayer.CustomDockerImage("ghcr.io/cosmos/relayer", "andrew-paths_update", rly.RlyDefaultUidGid),
@@ -46,11 +46,11 @@ func TestICS(t *testing.T) {
 
 	// Prep Interchain
 	const ibcPath = "ics-path"
-	ic := ibctest.NewInterchain().
+	ic := interchaintest.NewInterchain().
 		AddChain(provider).
 		AddChain(consumer).
 		AddRelayer(r, "relayer").
-		AddProviderConsumerLink(ibctest.ProviderConsumerLink{
+		AddProviderConsumerLink(interchaintest.ProviderConsumerLink{
 			Provider: provider,
 			Consumer: consumer,
 			Relayer:  r,
@@ -58,18 +58,18 @@ func TestICS(t *testing.T) {
 		})
 
 	// Log location
-	f, err := ibctest.CreateLogFile(fmt.Sprintf("%d.json", time.Now().Unix()))
+	f, err := interchaintest.CreateLogFile(fmt.Sprintf("%d.json", time.Now().Unix()))
 	require.NoError(t, err)
 	// Reporter/logs
 	rep := testreporter.NewReporter(f)
 	eRep := rep.RelayerExecReporter(t)
 
 	// Build interchain
-	err = ic.Build(ctx, eRep, ibctest.InterchainBuildOptions{
+	err = ic.Build(ctx, eRep, interchaintest.InterchainBuildOptions{
 		TestName:          t.Name(),
 		Client:            client,
 		NetworkID:         network,
-		BlockDatabaseFile: ibctest.DefaultBlockDatabaseFilepath(),
+		BlockDatabaseFile: interchaintest.DefaultBlockDatabaseFilepath(),
 
 		SkipPathCreation: false,
 	})
