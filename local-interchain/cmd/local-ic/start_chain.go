29fd48976d003bdf7bddf37aa311b126b25dd4c8
Reece Williams <reecepbcups@gmail.com>
2023-09-12 20:47:20 -0500

    cli: add app overrides for port and address

--- local-interchain/cmd/local-ic/start_chain.go
+++ local-interchain/cmd/local-ic/start_chain.go
@@ -8,6 +8,11 @@ import (
 	"github.com/strangelove-ventures/localinterchain/interchain"
 )
 
+const (
+	FlagAPIAddressOverride = "api-address"
+	FlagAPIPortOverride    = "api-port"
+)
+
 var startCmd = &cobra.Command{
 	Use:     "start <config.json>",
 	Aliases: []string{"s", "run"},
@@ -27,10 +32,19 @@ var startCmd = &cobra.Command{
 			configPath = filepath.Base(configPath)
 		}
 
-		interchain.StartChain(parentDir, configPath)
+		apiAddrOverride, _ := cmd.Flags().GetString(FlagAPIAddressOverride)
+		apiPortOverride, _ := cmd.Flags().GetUint16(FlagAPIPortOverride)
+
+		interchain.StartChain(parentDir, configPath, &interchain.AppOverrides{
+			AddressOverride: apiAddrOverride,
+			PortOverride:    apiPortOverride,
+		})
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
+
+	startCmd.Flags().String(FlagAPIAddressOverride, "", "Override the default API host address")
+	startCmd.Flags().Uint16(FlagAPIPortOverride, 0, "Override the default API port")
 }
