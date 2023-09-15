7422e57f5c0b69fa330f9f21107aa67ab21db5ec
Reece Williams <reecepbcups@gmail.com>
2023-09-14 22:29:32 -0500

    revert back to interchaintest/v7

--- examples/cosmos/chain_miscellaneous_test.go
+++ examples/cosmos/chain_miscellaneous_test.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"testing"
 
-	"github.com/strangelove-ventures/interchaintest/v8"
-	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
-	"github.com/strangelove-ventures/interchaintest/v8/ibc"
+	"github.com/strangelove-ventures/interchaintest/v7"
+	"github.com/strangelove-ventures/interchaintest/v7/chain/cosmos"
+	"github.com/strangelove-ventures/interchaintest/v7/ibc"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 )
