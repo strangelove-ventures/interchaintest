53423174438c92f2306b0901fb69258e6230d177
Reece Williams <reecepbcups@gmail.com>
2023-12-01 10:38:05 -0600

    cleanup bank queries

--- chain/cosmos/bank.go
+++ chain/cosmos/bank.go
@@ -36,12 +36,15 @@ func (tn *ChainNode) BankMultiSend(ctx context.Context, keyName string, addresse
 // GetBalance fetches the current balance for a specific account address and denom.
 // Implements Chain interface
 func (c *CosmosChain) GetBalance(ctx context.Context, address string, denom string) (sdkmath.Int, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return sdkmath.Int{}, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.Balance(ctx, &banktypes.QueryBalanceRequest{Address: address, Denom: denom})
+	res, err := banktypes.NewQueryClient(grpcConn).Balance(ctx, &banktypes.QueryBalanceRequest{Address: address, Denom: denom})
 	return res.Balance.Amount, err
 }
 
@@ -52,85 +55,109 @@ func (c *CosmosChain) BankGetBalance(ctx context.Context, address string, denom 
 
 // AllBalances fetches an account address's balance for all denoms it holds
 func (c *CosmosChain) BankAllBalances(ctx context.Context, address string) (types.Coins, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.AllBalances(ctx, &banktypes.QueryAllBalancesRequest{Address: address})
+	res, err := banktypes.NewQueryClient(grpcConn).AllBalances(ctx, &banktypes.QueryAllBalancesRequest{Address: address})
 	return res.GetBalances(), err
 }
 
 // BankDenomMetadata fetches the metadata of a specific coin denomination
 func (c *CosmosChain) BankDenomMetadata(ctx context.Context, denom string) (*banktypes.Metadata, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.DenomMetadata(ctx, &banktypes.QueryDenomMetadataRequest{Denom: denom})
+	res, err := banktypes.NewQueryClient(grpcConn).DenomMetadata(ctx, &banktypes.QueryDenomMetadataRequest{Denom: denom})
 	return &res.Metadata, err
 }
 
 func (c *CosmosChain) BankQueryDenomMetadataByQueryString(ctx context.Context, denom string) (*banktypes.Metadata, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.DenomMetadataByQueryString(ctx, &banktypes.QueryDenomMetadataByQueryStringRequest{Denom: denom})
+	res, err := banktypes.NewQueryClient(grpcConn).DenomMetadataByQueryString(ctx, &banktypes.QueryDenomMetadataByQueryStringRequest{Denom: denom})
 	return &res.Metadata, err
 }
 
 func (c *CosmosChain) BankQueryDenomOwners(ctx context.Context, denom string) ([]*banktypes.DenomOwner, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.DenomOwners(ctx, &banktypes.QueryDenomOwnersRequest{Denom: denom})
+	res, err := banktypes.NewQueryClient(grpcConn).DenomOwners(ctx, &banktypes.QueryDenomOwnersRequest{Denom: denom})
 	return res.DenomOwners, err
 }
 
 func (c *CosmosChain) BankQueryDenomsMetadata(ctx context.Context) ([]banktypes.Metadata, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.DenomsMetadata(ctx, &banktypes.QueryDenomsMetadataRequest{})
+	res, err := banktypes.NewQueryClient(grpcConn).DenomsMetadata(ctx, &banktypes.QueryDenomsMetadataRequest{})
 	return res.Metadatas, err
 }
 
 func (c *CosmosChain) BankQueryParams(ctx context.Context) (*banktypes.Params, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.Params(ctx, &banktypes.QueryParamsRequest{})
+	res, err := banktypes.NewQueryClient(grpcConn).Params(ctx, &banktypes.QueryParamsRequest{})
 	return &res.Params, err
 }
 
 func (c *CosmosChain) BankQuerySendEnabled(ctx context.Context, denoms []string) ([]*banktypes.SendEnabled, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.SendEnabled(ctx, &banktypes.QuerySendEnabledRequest{
+	res, err := banktypes.NewQueryClient(grpcConn).SendEnabled(ctx, &banktypes.QuerySendEnabledRequest{
 		Denoms: denoms,
 	})
 	return res.SendEnabled, err
 }
 
 func (c *CosmosChain) BankQuerySpendableBalance(ctx context.Context, address, denom string) (*types.Coin, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.SpendableBalanceByDenom(ctx, &banktypes.QuerySpendableBalanceByDenomRequest{
+	res, err := banktypes.NewQueryClient(grpcConn).SpendableBalanceByDenom(ctx, &banktypes.QuerySpendableBalanceByDenomRequest{
 		Address: address,
 		Denom:   denom,
 	})
@@ -138,41 +165,41 @@ func (c *CosmosChain) BankQuerySpendableBalance(ctx context.Context, address, de
 }
 
 func (c *CosmosChain) BankQuerySpendableBalances(ctx context.Context, address string) (*types.Coins, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.SpendableBalances(ctx, &banktypes.QuerySpendableBalancesRequest{Address: address})
+	res, err := banktypes.NewQueryClient(grpcConn).SpendableBalances(ctx, &banktypes.QuerySpendableBalancesRequest{Address: address})
 	return &res.Balances, err
 }
 
 func (c *CosmosChain) BankQueryTotalSupply(ctx context.Context, address string) (*types.Coins, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.TotalSupply(ctx, &banktypes.QueryTotalSupplyRequest{})
+	res, err := banktypes.NewQueryClient(grpcConn).TotalSupply(ctx, &banktypes.QueryTotalSupplyRequest{})
 	return &res.Supply, err
 }
 
 func (c *CosmosChain) BankQueryTotalSupplyOf(ctx context.Context, address string) (*types.Coin, error) {
-	queryClient, err := c.newBankQueryClient()
+	grpcConn, err := grpc.Dial(
+		c.GetNode().hostGRPCPort, grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
+	defer grpcConn.Close()
 
-	res, err := queryClient.SupplyOf(ctx, &banktypes.QuerySupplyOfRequest{Denom: address})
+	res, err := banktypes.NewQueryClient(grpcConn).SupplyOf(ctx, &banktypes.QuerySupplyOfRequest{Denom: address})
+
 	return &res.Amount, err
-}
-
-func (c *CosmosChain) newBankQueryClient() (banktypes.QueryClient, error) {
-	grpcAddress := c.getFullNode().hostGRPCPort
-	conn, err := grpc.Dial(grpcAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return nil, err
-	}
-
-	return banktypes.NewQueryClient(conn), nil
 }
