32e08bab0cb40f0d28d5534b14e9499f0f0c2b3a
Reece Williams <reecepbcups@gmail.com>
2023-12-04 09:51:12 -0600

    build TokenFactory off ChainNode

--- chain/cosmos/tokenfactory.go
+++ chain/cosmos/tokenfactory.go
@@ -13,14 +13,14 @@ import (
 // This token will be viewable by standard bank balance queries and send functionality.
 // Depending on the chain parameters, this may require a lot of gas (Juno, Osmosis) if the DenomCreationGasConsume param is enabled.
 // If not, the default implementation cost 10,000,000 micro tokens (utoken) of the chain's native token.
-func TokenFactoryCreateDenom(c *CosmosChain, ctx context.Context, user ibc.Wallet, denomName string, gas uint64) (string, string, error) {
+func (tn *ChainNode) TokenFactoryCreateDenom(ctx context.Context, user ibc.Wallet, denomName string, gas uint64) (string, string, error) {
 	cmd := []string{"tokenfactory", "create-denom", denomName}
 
 	if gas != 0 {
 		cmd = append(cmd, "--gas", strconv.FormatUint(gas, 10))
 	}
 
-	txHash, err := c.getFullNode().ExecTx(ctx, user.KeyName(), cmd...)
+	txHash, err := tn.ExecTx(ctx, user.KeyName(), cmd...)
 	if err != nil {
 		return "", "", err
 	}
@@ -29,48 +29,48 @@ func TokenFactoryCreateDenom(c *CosmosChain, ctx context.Context, user ibc.Walle
 }
 
 // TokenFactoryBurnDenom burns a tokenfactory denomination from the holders account.
-func TokenFactoryBurnDenom(c *CosmosChain, ctx context.Context, keyName, fullDenom string, amount uint64) (string, error) {
+func (tn *ChainNode) TokenFactoryBurnDenom(ctx context.Context, keyName, fullDenom string, amount uint64) (string, error) {
 	coin := strconv.FormatUint(amount, 10) + fullDenom
-	return c.getFullNode().ExecTx(ctx, keyName,
+	return tn.ExecTx(ctx, keyName,
 		"tokenfactory", "burn", coin,
 	)
 }
 
 // TokenFactoryBurnDenomFrom burns a tokenfactory denomination from any other users account.
 // Only the admin of the token can perform this action.
-func TokenFactoryBurnDenomFrom(c *CosmosChain, ctx context.Context, keyName, fullDenom string, amount uint64, fromAddr string) (string, error) {
-	return c.getFullNode().ExecTx(ctx, keyName,
+func (tn *ChainNode) TokenFactoryBurnDenomFrom(ctx context.Context, keyName, fullDenom string, amount uint64, fromAddr string) (string, error) {
+	return tn.ExecTx(ctx, keyName,
 		"tokenfactory", "burn-from", fromAddr, convertToCoin(amount, fullDenom),
 	)
 }
 
 // TokenFactoryChangeAdmin moves the admin of a tokenfactory token to a new address.
-func TokenFactoryChangeAdmin(c *CosmosChain, ctx context.Context, keyName, fullDenom, newAdmin string) (string, error) {
-	return c.getFullNode().ExecTx(ctx, keyName,
+func (tn *ChainNode) TokenFactoryChangeAdmin(ctx context.Context, keyName, fullDenom, newAdmin string) (string, error) {
+	return tn.ExecTx(ctx, keyName,
 		"tokenfactory", "change-admin", fullDenom, newAdmin,
 	)
 }
 
 // TokenFactoryForceTransferDenom force moves a token from 1 account to another.
 // Only the admin of the token can perform this action.
-func TokenFactoryForceTransferDenom(c *CosmosChain, ctx context.Context, keyName, fullDenom string, amount uint64, fromAddr, toAddr string) (string, error) {
-	return c.getFullNode().ExecTx(ctx, keyName,
+func (tn *ChainNode) TokenFactoryForceTransferDenom(ctx context.Context, keyName, fullDenom string, amount uint64, fromAddr, toAddr string) (string, error) {
+	return tn.ExecTx(ctx, keyName,
 		"tokenfactory", "force-transfer", convertToCoin(amount, fullDenom), fromAddr, toAddr,
 	)
 }
 
 // TokenFactoryMintDenom mints a tokenfactory denomination to the admins account.
 // Only the admin of the token can perform this action.
-func TokenFactoryMintDenom(c *CosmosChain, ctx context.Context, keyName, fullDenom string, amount uint64) (string, error) {
-	return c.getFullNode().ExecTx(ctx, keyName,
+func (tn *ChainNode) TokenFactoryMintDenom(ctx context.Context, keyName, fullDenom string, amount uint64) (string, error) {
+	return tn.ExecTx(ctx, keyName,
 		"tokenfactory", "mint", convertToCoin(amount, fullDenom),
 	)
 }
 
 // TokenFactoryMintDenomTo mints a token to any external account.
 // Only the admin of the token can perform this action.
-func TokenFactoryMintDenomTo(c *CosmosChain, ctx context.Context, keyName, fullDenom string, amount uint64, toAddr string) (string, error) {
-	return c.getFullNode().ExecTx(ctx, keyName,
+func (tn *ChainNode) TokenFactoryMintDenomTo(ctx context.Context, keyName, fullDenom string, amount uint64, toAddr string) (string, error) {
+	return tn.ExecTx(ctx, keyName,
 		"tokenfactory", "mint-to", toAddr, convertToCoin(amount, fullDenom),
 	)
 }
@@ -78,14 +78,14 @@ func TokenFactoryMintDenomTo(c *CosmosChain, ctx context.Context, keyName, fullD
 // TokenFactoryMetadata sets the x/bank metadata for a tokenfactory token. This gives the token more detailed information to be queried
 // by frontend UIs and other applications.
 // Only the admin of the token can perform this action.
-func TokenFactoryMetadata(c *CosmosChain, ctx context.Context, keyName, fullDenom, ticker, description string, exponent uint64) (string, error) {
-	return c.getFullNode().ExecTx(ctx, keyName,
+func (tn *ChainNode) TokenFactoryMetadata(ctx context.Context, keyName, fullDenom, ticker, description string, exponent uint64) (string, error) {
+	return tn.ExecTx(ctx, keyName,
 		"tokenfactory", "modify-metadata", fullDenom, ticker, description, strconv.FormatUint(exponent, 10),
 	)
 }
 
 // TokenFactoryGetAdmin returns the admin of a tokenfactory token.
-func TokenFactoryGetAdmin(c *CosmosChain, ctx context.Context, fullDenom string) (*QueryDenomAuthorityMetadataResponse, error) {
+func (c *CosmosChain) TokenFactoryGetAdmin(ctx context.Context, fullDenom string) (*QueryDenomAuthorityMetadataResponse, error) {
 	res := &QueryDenomAuthorityMetadataResponse{}
 	stdout, stderr, err := c.getFullNode().ExecQuery(ctx, "tokenfactory", "denom-authority-metadata", fullDenom)
 	if err != nil {
--- examples/cosmos/chain_miscellaneous_test.go
+++ examples/cosmos/chain_miscellaneous_test.go
@@ -347,13 +347,15 @@ func testTokenFactory(ctx context.Context, t *testing.T, chain *cosmos.CosmosCha
 	user := users[0]
 	user2 := users[1]
 
+	node := chain.GetNode()
+
 	subDenom := "ictest"
-	tfDenom, _, err := cosmos.TokenFactoryCreateDenom(chain, ctx, user, subDenom, 2500000)
+	tfDenom, _, err := node.TokenFactoryCreateDenom(ctx, user, subDenom, 2500000)
 	require.NoError(t, err)
 	require.Equal(t, tfDenom, "factory/"+user.FormattedAddress()+"/"+subDenom)
 
 	// modify metadata
-	stdout, err := cosmos.TokenFactoryMetadata(chain, ctx, user.KeyName(), tfDenom, "SYMBOL", "description here", 6)
+	stdout, err := node.TokenFactoryMetadata(ctx, user.KeyName(), tfDenom, "SYMBOL", "description here", 6)
 	t.Log(stdout, err)
 	require.NoError(t, err)
 
@@ -365,41 +367,41 @@ func testTokenFactory(ctx context.Context, t *testing.T, chain *cosmos.CosmosCha
 	require.Equal(t, md.Metadata.DenomUnits[1].Exponent, 6)
 
 	// mint tokens
-	_, err = cosmos.TokenFactoryMintDenom(chain, ctx, user.KeyName(), tfDenom, 1)
+	_, err = node.TokenFactoryMintDenom(ctx, user.KeyName(), tfDenom, 1)
 	require.NoError(t, err)
 	validateBalance(ctx, t, chain, user, tfDenom, 1)
 
 	// mint-to
-	_, err = cosmos.TokenFactoryMintDenomTo(chain, ctx, user.KeyName(), tfDenom, 3, user2.FormattedAddress())
+	_, err = node.TokenFactoryMintDenomTo(ctx, user.KeyName(), tfDenom, 3, user2.FormattedAddress())
 	require.NoError(t, err)
 	validateBalance(ctx, t, chain, user2, tfDenom, 3)
 
 	// force transfer 1 from user2 (3) to user1 (1)
-	_, err = cosmos.TokenFactoryForceTransferDenom(chain, ctx, user.KeyName(), tfDenom, 1, user2.FormattedAddress(), user.FormattedAddress())
+	_, err = node.TokenFactoryForceTransferDenom(ctx, user.KeyName(), tfDenom, 1, user2.FormattedAddress(), user.FormattedAddress())
 	require.NoError(t, err)
 	validateBalance(ctx, t, chain, user, tfDenom, 2)
 	validateBalance(ctx, t, chain, user2, tfDenom, 2)
 
 	// burn token from account
-	_, err = cosmos.TokenFactoryBurnDenomFrom(chain, ctx, user.KeyName(), tfDenom, 1, user.FormattedAddress())
+	_, err = node.TokenFactoryBurnDenomFrom(ctx, user.KeyName(), tfDenom, 1, user.FormattedAddress())
 	require.NoError(t, err)
 	validateBalance(ctx, t, chain, user, tfDenom, 1)
 
-	prevAdmin, err := cosmos.TokenFactoryGetAdmin(chain, ctx, tfDenom)
+	prevAdmin, err := chain.TokenFactoryGetAdmin(ctx, tfDenom)
 	require.NoError(t, err)
 	require.Equal(t, prevAdmin.AuthorityMetadata.Admin, user.FormattedAddress())
 
 	// change admin, then we will burn the token-from
-	_, err = cosmos.TokenFactoryChangeAdmin(chain, ctx, user.KeyName(), tfDenom, user2.FormattedAddress())
+	_, err = node.TokenFactoryChangeAdmin(ctx, user.KeyName(), tfDenom, user2.FormattedAddress())
 	require.NoError(t, err)
 
 	// validate new admin is set
-	tfAdmin, err := cosmos.TokenFactoryGetAdmin(chain, ctx, tfDenom)
+	tfAdmin, err := chain.TokenFactoryGetAdmin(ctx, tfDenom)
 	require.NoError(t, err)
 	require.Equal(t, tfAdmin.AuthorityMetadata.Admin, user2.FormattedAddress())
 
 	// burn
-	_, err = cosmos.TokenFactoryBurnDenomFrom(chain, ctx, user2.KeyName(), tfDenom, 1, user.FormattedAddress())
+	_, err = node.TokenFactoryBurnDenomFrom(ctx, user2.KeyName(), tfDenom, 1, user.FormattedAddress())
 	require.NoError(t, err)
 	validateBalance(ctx, t, chain, user, tfDenom, 0)
 
