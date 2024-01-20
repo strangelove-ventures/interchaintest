cb8031cf6a7679174659d5080f1e7bf3ef3197df
Reece Williams <reecepbcups@gmail.com>
2024-01-19 22:18:26 -0600

    rough base

--- examples/cosmos/rollkit_test.go
+++ examples/cosmos/rollkit_test.go
@@ -6,6 +6,9 @@ import (
 	"path"
 	"testing"
 
+	"cosmossdk.io/math"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/strangelove-ventures/interchaintest/v8"
 	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
 	"github.com/strangelove-ventures/interchaintest/v8/ibc"
@@ -15,24 +18,47 @@ import (
 )
 
 // https://rollkit.dev/tutorials/gm-world
-const (
+var (
 	// TODO: get https://github.com/rollkit/local-celestia-devnet/blob/main/entrypoint.sh into interchaintest
 	// 26650:26650 -p 26657:26657 -p 26658:26658 -p 26659:26659 -p 9090:9090 // 26650 runs th celestia-da server
-	// DockerImage   = "ghcr.io/celestiaorg/celestia-app"
-	DockerImage   = "ghcr.io/strangelove-ventures/heighliner/celestia"
-	DockerVersion = "v1.6.0"
+	// DockerImage   = "ghcr.io/strangelove-ventures/heighliner/celestia"
+	// DockerVersion = "v1.6.0"
+	// DADockerImage   = "ghcr.io/rollkit/celestia-da"
+	// DADockerVersion = "v0.12.6"
 
-	DADockerImage   = "ghcr.io/rollkit/celestia-da"
-	DADockerVersion = "v0.12.6"
+	// holds both the da and app binaries from heighliner so we can ignore docker mounting magic
+	DockerImage   = "reece/full-celestia"
+	DockerVersion = "v0.0.1"
 
 	// App & node has a celestia user with home dir /home/celestia
-	APP_PATH  = ".celestia-app"
-	NODE_PATH = "bridge"
+	hardcodedPath = "/var/cosmos-chain/celestia"
+	APP_PATH      = ".celestia-app"
+	NODE_PATH     = path.Join(hardcodedPath, "bridge")
+
+	// TODO: Set namespace and CELESTIA_CUSTOM env variable on bridge start
+	// echo 0000$(openssl rand -hex 8)
+	CELESTIA_NAMESPACE = "00007e5327f23637ed07"
+
+	startCmd = []string{
+		"celestia-da", "bridge", "init", "--node.store", NODE_PATH,
+	}
+
+	otherCmd = []string{
+		"celestia-da", "bridge", "start",
+		"--node.store", NODE_PATH,
+		"--gateway",
+		"--core.ip", "test-val-0-TestRollkitCelestia", // n.HostName()
+		"--keyring.accname", "validator",
+		"--gateway.addr", "0.0.0.0",
+		"--rpc.addr", "0.0.0.0",
+		"--da.grpc.namespace", CELESTIA_NAMESPACE,
+		"--da.grpc.listen", "0.0.0.0:26650",
+	}
 )
 
 func TestRollkitCelestia(t *testing.T) {
 	if testing.Short() {
-		t.Skip("skipping in short mode")
+		t.Skip("skipping in short mode.")
 	}
 
 	t.Parallel()
@@ -41,19 +67,13 @@ func TestRollkitCelestia(t *testing.T) {
 	numFullNodes := 0
 	coinDecimals := int64(6)
 
-	// https://github.com/rollkit/local-celestia-devnet/blob/main/entrypoint.sh
-
-	// configFileOverrides := make(map[string]any)
-	// configFileOverrides["config/config.toml"] = make(testutil.Toml)
-	// configFileOverrides["config/client.toml"] = make(testutil.Toml)
-
 	cfg := ibc.ChainConfig{
-		Name:           "celestia", // Type: Rollkit / Rollup / Celestia?
-		Denom:          "utia",
-		Type:           "cosmos",
-		GasPrices:      "0utia",
-		TrustingPeriod: "500h",
-		// HostPortOverride: ,
+		Name:                "celestia", // Type: Rollkit / Rollup / Celestia?
+		Denom:               "utia",
+		Type:                "cosmos",
+		GasPrices:           "0utia",
+		TrustingPeriod:      "500h",
+		HostPortOverride:    map[int]int{26650: 26650, 1317: 1317, 26656: 26656, 26657: 26657, 9090: 9090},
 		EncodingConfig:      nil,
 		SkipGenTx:           false,
 		CoinDecimals:        &coinDecimals,
@@ -74,7 +94,26 @@ func TestRollkitCelestia(t *testing.T) {
 		ConfigFileOverrides: testutil.Toml{"config/config.toml": testutil.Toml{
 			"index_all_keys": true,
 			"mode":           "validator", // be sure to only use 1 validator here
+			"tx_index": testutil.Toml{
+				"indexer": "kv", // TODO: since we execute queries against the validator (unsure if this works on a celestia validator node)
+			},
 		}},
+		SidecarConfigs: []ibc.SidecarConfig{
+			{
+				ValidatorProcess: true,
+				ProcessName:      "celestia-da",
+				Image: ibc.DockerImage{
+					Repository: DockerImage,
+					Version:    DockerVersion,
+					UidGid:     "1025:1025",
+				},
+				HomeDir:  hardcodedPath,
+				Ports:    []string{"26650"},
+				StartCmd: startCmd,
+				Env:      []string{},
+				PreStart: false,
+			},
+		},
 	}
 
 	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
@@ -109,17 +148,83 @@ func TestRollkitCelestia(t *testing.T) {
 		_ = ic.Close()
 	})
 
-	// var userFunds = math.NewInt(10_000_000_000)
-	// users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, chain)
-	// chainUser := users[0]
-	// fmt.Println("chainUser", chainUser)
+	var userFunds = math.NewInt(10_000_000_000)
+	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, chain)
+	chainUser := users[0]
+	fmt.Println("chainUser", chainUser)
 
-	nodePath := path.Join(chain.GetNode().HomeDir(), NODE_PATH)
-	fmt.Println("nodePath", nodePath)
+	fmt.Println("nodePaths", NODE_PATH)
 
-	// n := chain.GetNode()
-	// n.Exec()
+	// TODO: From the guide, after 1 block (20 seconds?)
+	n := chain.GetNode()
 
-	// TODO: get the genesis file from the node
-	// Start the celestia da bridge
+	// valCommonAddr, err := chain.GetAddress(ctx, "validator")
+	// require.NoError(t, err)
+
+	// // print valCommonAddr
+	// fmt.Println("valCommonAddr", string(valCommonAddr))
+
+	// // celestiavaloper (could also just query from staking?)
+	// valAddr, err := sdk.GetFromBech32(string(valCommonAddr), cfg.Bech32Prefix+"valoper")
+	// require.NoError(t, err)
+
+	// fmt.Println("valAddr", string(valAddr))
+
+	fmt.Println("n.ValidatorMnemonic", n.ValidatorMnemonic)
+
+	// qgb register validator
+	vals, err := chain.StakingQueryValidators(ctx, stakingtypes.BondStatusBonded)
+	require.NoError(t, err)
+
+	val := vals[0].OperatorAddress
+
+	fmt.Println("n.Sidecars", n.Sidecars[0])
+
+	// recover the validator key (from mnemonic) to the da layer home ( https://github.com/rollkit/local-celestia-devnet/blob/main/entrypoint.sh#L57 )
+	if err := RecoverKey(ctx, n, "validator", n.ValidatorMnemonic, path.Join(NODE_PATH, "keys")); err != nil {
+		t.Fatal(err)
+	}
+
+	sc := n.Sidecars[0]
+	if err := sc.CreateContainer(ctx); err != nil {
+		t.Fatal(err)
+	}
+	if err := sc.StartContainer(ctx); err != nil {
+		t.Fatal(err)
+	}
+
+	// waits for 20 seconds (2s blocks * 10)
+	testutil.WaitForBlocks(ctx, 10, chain)
+
+	stdout, stderr, err := sc.Exec(ctx, otherCmd, nil)
+	fmt.Println("stdout", stdout)
+	fmt.Println("stderr", stderr)
+	fmt.Println("err", err)
+
+	testutil.WaitForBlocks(ctx, 10, chain)
+
+	// register teh EVM address
+	//  # private key: da6ed55cb2894ac2c9c10209c09de8e8b9d109b910338d5bf3d747a7e1fc9eb9
+	txHash, err := n.ExecTx(ctx, "validator", "qgb", "register", val, "0x966e6f22781EF6a6A82BBB4DB3df8E225DfD9488", "--fees", "30000utia", "-b", "block", "-y")
+	require.NoError(t, err)
+
+	// convert txHash to response (this does not print, but works upon manually querying against the local node)
+	res, err := n.TxHashToResponse(ctx, txHash)
+	require.NoError(t, err)
+	fmt.Printf("res: %+v\n", res)
+
+	// wait for 100 blocks
+	testutil.WaitForBlocks(ctx, 100, chain)
+
+}
+
+func RecoverKey(ctx context.Context, tn *cosmos.ChainNode, keyName, mnemonic, homeDir string) error {
+	command := []string{
+		"sh",
+		"-c",
+		fmt.Sprintf(`echo %q | %s keys add %s --recover --keyring-backend %s --coin-type %s --home %s --output json`, mnemonic, tn.Chain.Config().Bin, keyName, keyring.BackendTest, tn.Chain.Config().CoinType, homeDir),
+	}
+
+	_, _, err := tn.Exec(ctx, command, nil)
+	return err
 }
