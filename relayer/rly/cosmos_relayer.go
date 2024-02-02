292af007252e327189f0d3c6120559c641a03824
Dan Kanefsky <56059752+boojamya@users.noreply.github.com>
2024-02-02 08:20:17 -0800

    Add create client options (#922)
    * add create client options
    
    * hermes
    
    * remove comment
    
    * fix comment
    
    (cherry picked from commit 401a81366d4fa181c4c0e8833a93b2d2966f56c7)
    
    # Conflicts:
    #	relayer/rly/cosmos_relayer.go

--- relayer/rly/cosmos_relayer.go
+++ relayer/rly/cosmos_relayer.go
@@ -139,18 +139,46 @@ func (commander) CreateChannel(pathName string, opts ibc.CreateChannelOptions, h
 	}
 }
 
-func (commander) CreateClients(pathName string, opts ibc.CreateClientOptions, homeDir string) []string {
-	return []string{
-		"rly", "tx", "clients", pathName, "--client-tp", opts.TrustingPeriod,
-		"--home", homeDir,
+func createClientOptsHelper(opts ibc.CreateClientOptions) []string {
+	var clientOptions []string
+	if opts.TrustingPeriod != "" {
+		clientOptions = append(clientOptions, "--client-tp", opts.TrustingPeriod)
 	}
+	if opts.TrustingPeriodPercentage != 0 {
+		clientOptions = append(clientOptions, "--client-tp-percentage", fmt.Sprint(opts.TrustingPeriodPercentage))
+	}
+	if opts.MaxClockDrift != "" {
+		clientOptions = append(clientOptions, "--max-clock-drift", opts.MaxClockDrift)
+	}
+
+	return clientOptions
 }
 
+func (commander) CreateClients(pathName string, opts ibc.CreateClientOptions, homeDir string) []string {
+	cmd := []string{"rly", "tx", "clients", pathName, "--home", homeDir}
+
+	clientOptions := createClientOptsHelper(opts)
+	cmd = append(cmd, clientOptions...)
+
+	return cmd
+}
+
+<<<<<<< HEAD
 func (commander) CreateClient(srcChainID, dstChainID, pathName string, opts ibc.CreateClientOptions, homeDir string) []string {
 	return []string{
 		"rly", "tx", "client", srcChainID, dstChainID, pathName, "--client-tp", opts.TrustingPeriod,
 		"--home", homeDir,
 	}
+=======
+// passing a value of 0 for customeClientTrustingPeriod will use default
+func (commander) CreateClient(pathName, homeDir string, opts ibc.CreateClientOptions) []string {
+	cmd := []string{"rly", "tx", "client", pathName, "--home", homeDir}
+
+	clientOptions := createClientOptsHelper(opts)
+	cmd = append(cmd, clientOptions...)
+
+	return cmd
+>>>>>>> 401a813 (Add create client options (#922))
 }
 
 func (commander) CreateConnections(pathName string, homeDir string) []string {
@@ -235,17 +263,20 @@ func (commander) GetClients(chainID, homeDir string) []string {
 }
 
 func (commander) LinkPath(pathName, homeDir string, channelOpts ibc.CreateChannelOptions, clientOpt ibc.CreateClientOptions) []string {
-	return []string{
+	cmd := []string{
 		"rly", "tx", "link", pathName,
 		"--src-port", channelOpts.SourcePortName,
 		"--dst-port", channelOpts.DestPortName,
 		"--order", channelOpts.Order.String(),
 		"--version", channelOpts.Version,
-		"--client-tp", clientOpt.TrustingPeriod,
 		"--debug",
-
 		"--home", homeDir,
 	}
+
+	clientOptions := createClientOptsHelper(clientOpt)
+	cmd = append(cmd, clientOptions...)
+
+	return cmd
 }
 
 func (commander) RestoreKey(chainID, keyName, coinType, mnemonic, homeDir string) []string {
