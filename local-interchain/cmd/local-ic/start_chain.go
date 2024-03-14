c1633abb80b3e72c6e294635409a580b40001a77
Reece Williams <reecepbcups@gmail.com>
2024-03-14 11:20:11 -0500

    feat: support remote JSON configs

--- local-interchain/cmd/local-ic/start_chain.go
+++ local-interchain/cmd/local-ic/start_chain.go
@@ -25,7 +25,11 @@ var startCmd = &cobra.Command{
 	Use:     "start <config.json>",
 	Aliases: []string{"s", "run"},
 	Short:   "Starts up the chain of choice with the config name",
-	Args:    cobra.ExactArgs(1),
+	Example: `local-ic start base_ibc
+ICTEST_HOME=. local-ic start mychain
+local-ic start https://gist.githubusercontent.com/Reecepbcups/70bf59c82c797ead9a5430b8b9d8d852/raw/cecc7be35bcec8b976a5d92e78fd6d56de2e1aa1/cosmoshub_localic_config.json
+`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		configPath := args[0]
 		parentDir := GetDirectory()
@@ -40,6 +44,28 @@ var startCmd = &cobra.Command{
 			configPath = filepath.Base(configPath)
 		}
 
+		var config *types.Config
+		var err error
+
+		if strings.HasPrefix(configPath, "http") {
+			config, err = interchain.LoadConfigFromURL(configPath)
+			if err != nil {
+				panic(err)
+			}
+
+			// last part of the URL to be the test name
+			configPath = configPath[strings.LastIndex(configPath, "/")+1:]
+		} else {
+			config, err = interchain.LoadConfig(parentDir, configPath)
+			if err != nil {
+				// try again with .json, then if it still fails - panic
+				config, err = interchain.LoadConfig(parentDir, configPath+".json")
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+
 		apiAddr, _ := cmd.Flags().GetString(FlagAPIAddressOverride)
 		apiPort, _ := cmd.Flags().GetUint16(FlagAPIPortOverride)
 
@@ -51,6 +77,7 @@ var startCmd = &cobra.Command{
 		interchain.StartChain(parentDir, configPath, &types.AppStartConfig{
 			Address: apiAddr,
 			Port:    apiPort,
+			Cfg:     config,
 
 			Relayer: types.Relayer{
 				DockerImage: types.DockerImage{
