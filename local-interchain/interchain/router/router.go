4583ad1d32aa7d6c011462991383c8d95c427a9e
Reece Williams <reecepbcups@gmail.com>
2024-08-06 08:25:43 -0500

    chain_registry_assets optional endpoint

--- local-interchain/interchain/router/router.go
+++ local-interchain/interchain/router/router.go
@@ -52,6 +52,14 @@ func NewRouter(
 		log.Printf("chain_registry.json not found in %s, not exposing endpoint.", wd)
 	}
 
+	chainRegistryAssetsFile := filepath.Join(wd, "chain_registry_assets.json")
+	if _, err := os.Stat(chainRegistryAssetsFile); err == nil {
+		crH := handlers.NewChainRegistry(chainRegistryAssetsFile)
+		r.HandleFunc("/chain_registry_assets", crH.GetChainRegistry).Methods(http.MethodGet)
+	} else {
+		log.Printf("chain_registry_assets.json not found in %s, not exposing endpoint.", wd)
+	}
+
 	actionsH := handlers.NewActions(ctx, ic, cosmosChains, vals, relayer, eRep, authKey)
 	r.HandleFunc("/", actionsH.PostActions).Methods(http.MethodPost)
 
