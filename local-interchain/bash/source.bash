fc39d09674c57e70e193f2ca89911ad0d8064c5b
Reece Williams <reecepbcups@gmail.com>
2024-08-10 17:11:17 -0500

    upload files + cw,

--- local-interchain/bash/source.bash
+++ local-interchain/bash/source.bash
@@ -1,5 +1,15 @@
 # IMPORT ME WITH: source <(curl -s https://raw.githubusercontent.com/strangelove-ventures/interchaintest/main/local-interchain/bash/source.bash)
 
+# exitIfEmpty "$someKey" someKey
+function ICT_exitIfEmpty() {
+    if [ -z "$1" ]; then
+        echo "Exiting because ${2} is empty"
+        exit 1
+    fi
+}
+
+# === BASE ===
+
 # ICT_MAKE_REQUEST http://127.0.0.1:8080 localjuno-1 "q" "bank total"
 ICT_MAKE_REQUEST() {
     API=$1
@@ -13,15 +23,6 @@ ICT_MAKE_REQUEST() {
     DATA=`echo $DATA | sed "s/MYCOMMAND/$COMMAND/g"`
 
     curl "$API" -ss --no-progress-meter --header "Content-Type: application/json" -X POST -d "$DATA"
-}
-
-ICT_POLL_FOR_START() {
-    API=$1
-
-    attempt_counter=1
-    ATTEMPTS_MAX=$2
-
-    curl --head -X GET --retry $ATTEMPTS_MAX --retry-connrefused --retry-delay 3 $API
 }
 
 # ICT_QUERY "http://localhost:8080" "localjuno-1" "bank balances juno10r39fueph9fq7a6lgswu4zdsg8t3gxlq670lt0"
@@ -46,69 +47,6 @@ ICT_SH_EXEC() {
     CHAIN_ID=$2
     CMD=$3 # can be multiple words
     ICT_MAKE_REQUEST "$API" $CHAIN_ID "exec" "$CMD"
-}
-
-# ICT_FAUCET_REQUEST "http://localhost:8080" "localjuno-1" "1000000000ujuno" "juno1qk7zqy3k2v3jx2zq2z2zq2zq2zq2zq2zq2zq"
-ICT_FAUCET_REQUEST() {
-    API=$1
-    CHAIN_ID=$2
-    AMOUNT=$3
-    ADDRESS=$4
-    ICT_MAKE_REQUEST $API $CHAIN_ID "faucet" "amount=$AMOUNT;address=$ADDRESS"
-}
-
-# === COSMWASM ===
-
-# ICT_DUMP_CONTRACT_STATE "http://localhost:8080" "localjuno-1" "cosmos1contractaddress" "100"
-ICT_DUMP_CONTRACT_STATE() {
-    API=$1
-    CHAIN_ID=$2
-    CONTRACT=$3
-    HEIGHT=$4
-
-    ICT_MAKE_REQUEST $API $CHAIN_ID "recover-key" "contract=$CONTRACT;height=$HEIGHT"
-}
-
-# === OTHER ===
-
-# ICT_KILL_ALL "http://localhost:8080" "localjuno-1"
-# (Kills all running, keeps local-ic process. `killall local-ic` to kill that as well)
-ICT_KILL_ALL() {
-    API=$1
-    CHAIN_ID=$2
-    ICT_MAKE_REQUEST $API $CHAIN_ID "kill-all" ""
-}
-
-# ICT_GET_PEER "http://localhost:8080" "localjuno-1"
-ICT_GET_PEER() {
-    API=$1
-    CHAIN_ID=$2
-
-    if [[ $API != */info ]]; then
-        API="$API/info"
-    fi
-
-    curl -G -d "chain_id=$CHAIN_ID" -d "request=peer" $API
-}
-
-# ICT_ADD_FULL_NODE http://127.0.0.1:8080 "localjuno-1" "1"
-ICT_ADD_FULL_NODE() {
-    API=$1
-    CHAIN_ID=$2
-    AMOUNT=$3
-
-    ICT_MAKE_REQUEST $API $CHAIN_ID "add-full-nodes" "amount=$AMOUNT"
-}
-
-# ICT_RECOVER_KEY "http://localhost:8080" "localjuno-1" "mykey" "my mnemonic string here"
-ICT_RECOVER_KEY() {
-    API=$1
-    CHAIN_ID=$2
-    KEYNAME=$3
-    shift 3 # get the 4th argument and up as the command
-    MNEMONIC="$*"
-
-    ICT_MAKE_REQUEST $API $CHAIN_ID "recover-key" "keyname=$KEYNAME;mnemonic=$MNEMONIC"
 }
 
 # === RELAYER ===
@@ -149,4 +87,97 @@ ICT_RELAYER_CHANNELS() {
     ICT_MAKE_REQUEST $API $CHAIN_ID "get_channels" ""
 }
 
+# === COSMWASM ===
+
+# ICT_WASM_DUMP_CONTRACT_STATE "http://localhost:8080" "localjuno-1" "cosmos1contractaddress" "100"
+ICT_WASM_DUMP_CONTRACT_STATE() {
+    API=$1
+    CHAIN_ID=$2
+    CONTRACT=$3
+    HEIGHT=$4
+
+    ICT_MAKE_REQUEST $API $CHAIN_ID "recover-key" "contract=$CONTRACT;height=$HEIGHT"
+}
+
+# ICT_WASM_STORE_FILE "http://localhost:8080" "localjuno-1" "/host/absolute/path.wasm" "keyName"
+# returns the code_id of the uploaded contract
+ICT_WASM_STORE_FILE() {
+    API=$1
+    CHAIN_ID=$2
+    FILE=$3
+    KEYNAME=$4
+
+    DATA=`printf '{"chain_id":"%s","file_path":"%s","key_name":"%s"}' $CHAIN_ID $FILE $KEYNAME`
+    curl "$API/upload" --header "Content-Type: application/json" --header "Upload-Type: cosmwasm" -X POST -d "$DATA"
+}
+
+# === OTHER ===
+
+# ICT_POLL_FOR_START "http://localhost:8080" 50
+ICT_POLL_FOR_START() {
+    API=$1
+    ATTEMPTS_MAX=$2
+
+    curl --head -X GET --retry $ATTEMPTS_MAX --retry-connrefused --retry-delay 3 $API
+}
+
+# ICT_KILL_ALL "http://localhost:8080" "localjuno-1"
+# (Kills all running, keeps local-ic process. `killall local-ic` to kill that as well)
+ICT_KILL_ALL() {
+    API=$1
+    CHAIN_ID=$2
+    ICT_MAKE_REQUEST $API $CHAIN_ID "kill-all" ""
+}
+
+# ICT_GET_PEER "http://localhost:8080" "localjuno-1"
+ICT_GET_PEER() {
+    API=$1
+    CHAIN_ID=$2
+
+    if [[ $API != */info ]]; then
+        API="$API/info"
+    fi
+
+    curl -G -d "chain_id=$CHAIN_ID" -d "request=peer" $API
+}
+
+# ICT_FAUCET_REQUEST "http://localhost:8080" "localjuno-1" "1000000000ujuno" "juno1qk7zqy3k2v3jx2zq2z2zq2zq2zq2zq2zq2zq"
+ICT_FAUCET_REQUEST() {
+    API=$1
+    CHAIN_ID=$2
+    AMOUNT=$3
+    ADDRESS=$4
+    ICT_MAKE_REQUEST $API $CHAIN_ID "faucet" "amount=$AMOUNT;address=$ADDRESS"
+}
+
+# ICT_ADD_FULL_NODE http://127.0.0.1:8080 "localjuno-1" "1"
+ICT_ADD_FULL_NODE() {
+    API=$1
+    CHAIN_ID=$2
+    AMOUNT=$3
+
+    ICT_MAKE_REQUEST $API $CHAIN_ID "add-full-nodes" "amount=$AMOUNT"
+}
+
+# ICT_RECOVER_KEY "http://localhost:8080" "localjuno-1" "mykey" "my mnemonic string here"
+ICT_RECOVER_KEY() {
+    API=$1
+    CHAIN_ID=$2
+    KEYNAME=$3
+    shift 3 # get the 4th argument and up as the command
+    MNEMONIC="$*"
+
+    ICT_MAKE_REQUEST $API $CHAIN_ID "recover-key" "keyname=$KEYNAME;mnemonic=$MNEMONIC"
+}
+
+# ICT_STORE_FILE "http://localhost:8080" "localjuno-1" "/host/absolute/path"
+# Uploads any arbitrary host file to the chain node.
+ICT_STORE_FILE() {
+    API=$1
+    CHAIN_ID=$2
+    FILE=$3
+
+    DATA=`printf '{"chain_id":"%s","file_path":"%s"}' $CHAIN_ID $FILE`
+    curl "$API/upload" --header "Content-Type: application/json" -X POST -d "$DATA"
+}
 
