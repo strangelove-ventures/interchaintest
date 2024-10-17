aa44ddb07b8f0729d8192b76f754b1a75d83e2e8
Dan Kanefsky <daniel.kanefsky@gmail.com>
2024-10-17 12:32:52 -0700

    fix: unit test

--- interchain_test.go
+++ interchain_test.go
@@ -502,7 +502,7 @@ func TestInterchain_ConflictRejection(t *testing.T) {
 		chains, err := cf.Chains(t.Name())
 		require.NoError(t, err)
 
-		require.PanicsWithError(t, "a chain with name g already exists", func() {
+		require.PanicsWithError(t, "a chain with name c already exists", func() {
 			_ = interchaintest.NewInterchain().AddChain(chains[0]).AddChain(chains[1])
 		})
 	})
