004d1be4081194eb9e54c8031f43d9691bac0900
Steve Miskovetz <steve.miskovetz@gmail.com>
2025-01-07 10:18:40 -0700

    fix temp change

--- chain/namada/namada_chain.go
+++ chain/namada/namada_chain.go
@@ -556,7 +556,7 @@ func (c *NamadaChain) SendIBCTransfer(ctx context.Context, channelID, keyName st
 	}
 	tx.Packet.TimeoutTimestamp = ibc.Nanoseconds(timeoutNano)
 
-	return ibc.Tx{}, nil
+	return tx, nil
 }
 
 // Shielded transfer (shielded account to shielded account) on Namada.
