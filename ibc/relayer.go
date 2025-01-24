90100c337558e34d532c7470732449ce31551b63
mmsqe <mavis@crypto.com>
2025-01-24 13:14:19 +0800

    avoid invalid duration for client-tp

--- ibc/relayer.go
+++ ibc/relayer.go
@@ -292,7 +292,9 @@ type CreateClientOptions struct {
 
 // empty values will use the relayer defaults.
 func DefaultClientOpts() CreateClientOptions {
-	return CreateClientOptions{}
+	return CreateClientOptions{
+		TrustingPeriod: "0",
+	}
 }
 
 func (opts CreateClientOptions) Validate() error {
